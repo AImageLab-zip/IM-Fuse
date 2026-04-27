e7b4e64377a20b78c48d4f2b77df4d264ed2d2ee
homarone00 <269868@studenti.unimore.it>
2026-04-27 08:52:31 +0200

    removed useless files

--- ProtoKD/code/csv_to_txt.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-input_path = '/homes/ocarpentiero/IM-Fuse/MaM/datalist/test15splits.csv'
-output_path = '/homes/ocarpentiero/IM-Fuse/MaM/datalist/test.txt'
-
-df = pd.read_csv(input_path)
-sub_list = df['case'].to_list()
-with open(output_path,'w') as f:
-    f.writelines([sub + '\n' for sub in sub_list])
