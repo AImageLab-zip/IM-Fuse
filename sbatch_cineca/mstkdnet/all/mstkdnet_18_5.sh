7f12c2769f0d38b38d2bc3a052b9f93b23983f3a
homarone00 <269868@studenti.unimore.it>
2026-07-15 16:55:55 +0200

    updated partition and qos of cineca sbatch files

--- sbatch_cineca/mstkdnet/all/mstkdnet_18_5.sh
+++ sbatch_cineca/mstkdnet/all/mstkdnet_18_5.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=MSTKDNET18_FOLD_5
-#SBATCH --partition=boost_qos_lprod
+#SBATCH --partition=boost_usr_prod
+#SBATCH --qos=boost_qos_lprod
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
--- sbatch_cineca/robustseg/all/robustseg_18_3.sh
+++ sbatch_cineca/robustseg/all/robustseg_18_3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=ROBSEG18_FOLD_3
-#SBATCH --partition=boost_qos_lprod
+#SBATCH --partition=boost_usr_prod
+#SBATCH --qos=boost_qos_lprod
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
